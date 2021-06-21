f1edc7bd8d0be1fab482a2823027f38f8915eba1
Yaroslav <webdev1232@gmail.com>
2021-06-21 15:21:51 +0300

    production, txt, money: only

--- bankConsoleBot.cpp
+++ bankConsoleBot.cpp
@@ -7,26 +7,16 @@
 
 using namespace std;
 
-void addUsers();
+int userLastId = 0;
+fstream fs;
+string BD[20][8][2];
 void addUserInDB(int userID, string userName, string userSurname, string userPatronymic, string userAge, string userCartMoney, string userCartNumber, string userCartPin);
 string* parseUserLineDB(string lineDB, string delimiter);
 void printQuestionInConsole(string printedLine);
 void readBD();
-int userLastId = 0;
-fstream fs;
-string BD[20][8][2];
 void registerUser();
-int startScreen();
-void creditScreen();
-void manager();
 void cartInfoScreen();
 void cartScreen();
-
-void addUsers() {
-    addUserInDB(0,"Filatov", "Albert", "Vyacheslavovich", "32", "0", "1463", "14");
-    addUserInDB(1,"Kalashnikov", "Ostap", "Maksimovich", "26", "0", "4286", "27");
-    addUserInDB(2, "Blinova", "Birgit", "Mikhailovna", "26", "0", "4789", "94");
-}
 
 void addUserInDB(int userID, string userName, string userSurname, string userPatronymic, string userAge, string userCartMoney, string userCartNumber, string userCartPin) {
     string maketUser = "\nid:"+ to_string(userID) +"|name:" + userName + "|surname:"+ userSurname +"|patronymic:"+ userPatronymic +"|age:"+ userAge +"|cartMoney:"+ userCartMoney +"|cartNumber:"+ userCartNumber +"|cartPin:"+ userCartPin;
@@ -37,7 +27,7 @@ void addUserInDB(int userID, string userName, string userSurname, string userPat
     fs.close();
 }
 
-/* спизжено */
+
 string* parseUserLineDB(string lineDB, string delimiter) {
     size_t pos = 0;
     string token;
@@ -67,11 +57,7 @@ void readBD() {
                 userDataFields = parseUserLineDB(lineDB, "|");
 
                 for (int i = 0; i < 8; i++) {
-                    //cout << "строка " << lineIndex << " скобка" << i << endl;
                     userDataValue = parseUserLineDB(userDataFields[i], ":");
-                    //cout << userDataValue[0] << endl;
-                    //cout << userDataValue[1] << endl;
-
                     BD[lineIndex][i][0] = userDataValue[0];
                     BD[lineIndex][i][1] = userDataValue[1];
                 }
@@ -111,7 +97,7 @@ void registerUser() {
     cin >> userAge;
     if (stoi(userAge) < 18) {
         printQuestionInConsole("Ваш возраст меньше 18.");
-        return manager();
+        return cartScreen();
     }
     srand(time(NULL));
     cartNumber = to_string(1000 + rand() % 9000);
@@ -136,60 +122,7 @@ void registerUser() {
     userLastId++;
     clearBD();
     saveBD();
-    manager();
-}
-
-
-
-void creditScreen() {
-    int userSelected;
-    printQuestionInConsole("Выберите пункт меню \n 1) Получить кредит \n 2) Вывод кредитной операции \n 3) Информация по текущему кредиту \n 4) Вернуться на главную");
-    cin >> userSelected;
-    if (userSelected == 1 || userSelected == 2 || userSelected == 3) {
-        switch (userSelected)
-        {
-        case 1: // кредит - получение
-            cout << "1";
-            break;
-        case 2: // кредит - история
-            cout << "2";
-            break;
-        case 3: // кредит - информация
-            cout << "3";
-            break;
-        case 5: // кредит - главная
-            manager();
-            break;
-        default:
-            break;
-        }
-    }
-}
-
-void manager() {
-    switch (startScreen())
-    {
-    case 1: // карта
-        cartScreen();
-        break;
-    case 2: // кредит
-        creditScreen();
-        break;
-    default:
-        break;
-    }
-}
-
-int startScreen() {
-    int userSelected;
-    printQuestionInConsole("Выберите пункт меню \n 1) Операции с картой \n 2) Кредитные операции");
-    cin >> userSelected;
-    if (userSelected == 1 || userSelected == 2) {
-        return userSelected;
-    }
-    else {
-        return startScreen();
-    }
+    cartScreen();
 }
 
 void printQuestionInConsole(string printedLine) {
@@ -205,7 +138,7 @@ void cartOperations(int userSelected, int DBIndex) {
     {
     case 1: // карта - инфо - узнать баланс
         cout << BD[DBIndex][5][1] << " руб" << endl;
-        manager();
+        cartScreen();
         break;
     case 2: // карта - инфо - снять
         int withdrawal;
@@ -222,7 +155,7 @@ void cartOperations(int userSelected, int DBIndex) {
         cout << BD[DBIndex][5][1] << " руб" << endl;
         clearBD();
         saveBD();
-        manager();
+        cartScreen();
         break;
     case 3: // карта - инфо - внести
         int deposit;
@@ -239,13 +172,11 @@ void cartOperations(int userSelected, int DBIndex) {
         }
         clearBD();
         saveBD();
-        manager();
-        break;
-    case 4: // карта - инфо - назад
         cartScreen();
         break;
-    case 5: // карта - инфо - главная
-        manager();
+    case 4: // карта - инфо - главная
+        cartScreen();
+        break;
         break;
     default:
         break;
@@ -260,9 +191,12 @@ void cartInfoScreen() {
     cin >> uCartNumber;
     printQuestionInConsole("Введите пароль");
     cin >> uCartPass;
-    printQuestionInConsole("Выберите тип операции \n 1) Узнать баланс \n 2) Снять наличные \n 3) Внести наличные \n 4) Назад \n 5) Вернутся на главную");
+    printQuestionInConsole("Выберите тип операции \n 1) Узнать баланс \n 2) Снять наличные \n 3) Внести наличные \n 4) Вернутся на главную");
     cin >> userSelected;
 
+    if (userSelected == 4) {
+        return cartScreen();
+    }
     for (int i = 0; i < 20; i++) {
         if (BD[i][1][1] != "") {
             if (BD[i][6][1] == uCartNumber) {
@@ -279,10 +213,7 @@ void cartInfoScreen() {
     }
     if (!hasUser) {
         cout << "Не найден пользователь с данной картой\n";
-        //cartInfoScreen();
-    }
-    else {
-        //cartOperations(userSelected, bankDB.allUsers()[userCurrentUserIndex]);
+        cartScreen();
     }
 }
 
@@ -300,7 +231,7 @@ void cartScreen() {
             cartInfoScreen();
             break;
         case 3: // карта - главная
-            manager();
+            cartScreen();
             break;
         default:
             break;
@@ -314,6 +245,9 @@ void cartScreen() {
 int main()
 {
     setlocale(LC_ALL, "Russian");
+
+    cout << "Hello World!\n" << endl;
+
     readBD();
-    manager();
+    cartScreen();
 }
